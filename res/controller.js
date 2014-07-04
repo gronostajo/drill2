d842ae174bc726d5ee2b5c0394e89d60e6ed95ca
Krzysiek Smialek <maxi.cx75@gmail.com>
2014-07-04 23:50:16 +0200

    Fixed newline handling when Markdown is disabled

--- res/controller.js
+++ res/controller.js
@@ -432,9 +432,18 @@
 		};
 	}])
 
-	.filter('maybeMarkdown', ['markdownFilter', '$sce', function(markdownFilter, $sce) {
+	.filter('paragraphs', ['$sce', function($sce) {
+		return function(str) {
+			if (!str) return '';
+			var lines = str.split(/\s*(\r?\n)(\r?\n\s)*/);
+			var html = '<p>' + lines.join('</p><p>') + '</p>';
+			return $sce.trustAsHtml(html);
+		};
+	}])
+
+	.filter('maybeMarkdown', ['markdownFilter', 'paragraphsFilter', function(markdownFilter, paragraphsFilter) {
 		return function(str, $scope) {
-			return $scope.config.markdown ? markdownFilter(str) : $sce.trustAsHtml(str);
+			return $scope.config.markdown ? markdownFilter(str) : paragraphsFilter(str);
 		};
 	}]);
 

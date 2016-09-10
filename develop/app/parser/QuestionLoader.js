51223df26a7ef5257cd24e9499c339e392e4b949
Travis <travis@avensome.net>
2016-09-10 00:35:38 +0000

    Deploy develop: a4cad112e2337f43ff830169f5553378f4fcd08e

--- develop/app/parser/QuestionLoader.js
+++ develop/app/parser/QuestionLoader.js
@@ -1,11 +1,10 @@
-angular.module('DrillApp').service('QuestionLoader', function ($q, SafeEvalService, LegacyParser) {
+angular.module('DrillApp').service('QuestionLoader', function ($q, SafeEvalService, LegacyParser, OptionsBlockProcessor) {
     //
     // TODO extract this to a worker
     //
     var ret = {
         loadFromString: function (input) {
             var bankInfo = {};
-            var config = {};
 
             var qs = input.split(/(?:\r?\n){2,}/);
 
@@ -20,94 +19,22 @@ angular.module('DrillApp').service('QuestionLoader', function ($q, SafeEvalServi
                 repeatIncorrect: false,
                 explain: 'optional'
             };
-            var expl = false;
+
+            var config = {};
+            var log = [];
 
             var matched = /<options>\s*(\{(?:.|\n|\r)*})\s*/i.exec(qs[qs.length - 1]);
             if (matched) {
                 qs.pop();
-
-                try {
-                    var loaded = JSON.parse(matched[1]);
-                } catch (e) {
-                    console.error('Invalid <options> object:', matched[1]);
-                }
-
-                for (var key in loaded) {
-                    if (key == 'explanations') {
-                        expl = loaded[key];
-                    }
-                    else if (options.hasOwnProperty(key)) {
-                        options[key] = loaded[key];
-                    }
-                }
+                var optionsBlockContents = matched[1];
+                config = OptionsBlockProcessor.process(optionsBlockContents, function (msg) {
+                    log.push(msg);
+                });
             }
 
-            switch (options.format) {
-                case 'legacy':
-                case '2':
-                case '2.1':
-                    bankInfo.fileFormat = options.format;
-                    break;
-
-                default:
-                    bankInfo.fileFormat = 'unknown';
-                    break;
-            }
-
-            config.markdownReady = !!options.markdown;
-            config.markdown = config.markdownReady;
-
-            config.mathjaxReady = !!options.mathjax;
-            config.mathjax = config.mathjaxReady;
-
-            config.customGrader = false;
-
-            if ((options.grading == 'perQuestion') || (options.grading == 'perAnswer')) {
-                // for built-in graders, just accept them
-                config.gradingMethod = options.grading;
-            }
-            else {
-                //noinspection JSDuplicatedDeclaration
-                matched = /^custom: *(.+)$/.exec(options.grading);
-                if (matched) {
-                    try {
-                        SafeEvalService.eval(matched[1], function (id) {
-                            return (id == 'total') ? 3 : 1;
-                        });
-                        config.gradingMethod = 'custom';
-                        config.customGrader = matched[1];
-                    }
-                    catch (ex) {
-                        console.error('Custom grader caused an error when testing.');
-                    }
-                }
-                else {
-                    config.gradingMethod = 'perAnswer';
-                }
-            }
-
-            config.gradingRadical = options.radical ? '1' : '0';
-            config.gradingPPQ = parseInt(options.ptsPerQuestion);
-
-            var secs = (parseInt(options.timeLimit) / 5) * 5;
-            if (!secs) {
-                config.timeLimitEnabled = false;
-                config.timeLimitSecs = 60;
-            }
-            else {
-                config.timeLimitEnabled = true;
-                config.timeLimitSecs = secs;
-            }
-
-            config.repeatIncorrect = !!options.repeatIncorrect;
-
-            if (expl && /summary|optional|always/i.exec(options.explain)) {
-                config.explain = options.explain.toLowerCase();
-            }
-            else if (expl) {
-                config.explain = 'optional';
-            }
-            config.showExplanations = (config.explain == 'always');
+            bankInfo.fileFormat = config.fileFormat || 'legacy';
+            var expl = config.explanations;
+            delete config.explanations;
 
             var questionsString = qs.join('\n\n');
             var parsingResult = LegacyParser.parse(questionsString);
@@ -132,7 +59,7 @@ angular.module('DrillApp').service('QuestionLoader', function ($q, SafeEvalServi
                 bankInfo: bankInfo,
                 config: config,
                 loadedQuestions: loadedQuestions,
-                log: parsingResult.log
+                log: log.concat(parsingResult.log)
             });
         }
     };

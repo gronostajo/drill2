3316c099e59df15bd60b6ae2cec9b3a79f009a5b
Travis <travis@avensome.net>
2016-09-11 11:04:27 +0000

    Deploy develop: d7696fffa85bbf4002eb190d279289db48c0198c

--- develop/app/parser/QuestionLoader.js
+++ develop/app/parser/QuestionLoader.js
@@ -1,62 +1,29 @@
-angular.module('DrillApp').service('QuestionLoader', function ($q, SafeEvalService, LegacyParser, OptionsBlockProcessor) {
-    //
-    // TODO extract this to a worker
-    //
-    var ret = {
-        loadFromString: function (input) {
-            var qs = input.split(/(?:\r?\n){2,}/);
+angular.module('DrillApp').service('QuestionLoader', function($q, QuestionParser) {
+  return new ((function() {
+    function _Class() {}
 
-            var bankInfo = {};
-            var config = {};
-            var log = [];
-
-            var matched = /<options>\s*(\{(?:.|\n|\r)*})\s*/i.exec(qs[qs.length - 1]);
-            if (matched) {
-                qs.pop();
-                var optionsBlockContents = matched[1];
-                config = OptionsBlockProcessor.process(optionsBlockContents, function (msg) {
-                    log.push(msg);
-                });
-            }
-
-            bankInfo.fileFormat = config.fileFormat || 'legacy';
-            var expl = config.explanations;
-            delete config.explanations;
-
-            var questionsString = qs.join('\n\n');
-            var parsingResult = LegacyParser.parse(questionsString);
-            var loadedQuestions = parsingResult.questions;
-
-            bankInfo.explanationsAvailable = false;
-            if (expl) {
-                for (var q = 0; q < loadedQuestions.length; q++) {
-                    loadedQuestions[q].loadExplanation(expl);
-                    if (loadedQuestions[q].hasExplanations) {
-                        bankInfo.explanationsAvailable = true;
-                    }
-                }
-            }
-
-            bankInfo.questionCount = loadedQuestions.length;
-
-            return $q.resolve({
-                bankInfo: bankInfo,
-                config: config,
-                loadedQuestions: loadedQuestions,
-                log: log.concat(parsingResult.log)
-            });
-        }
+    _Class.prototype.loadFromString = function(input) {
+      var bankInfo, e, error, log, options, questions, ref;
+      try {
+        ref = QuestionParser.parse(input), questions = ref.questions, options = ref.options, log = ref.log;
+        bankInfo = {
+          fileFormat: options.fileFormat,
+          explanationsAvailable: options.explanationsAvailable,
+          questionCount: questions.length
+        };
+        return $q.resolve({
+          bankInfo: bankInfo,
+          config: options,
+          loadedQuestions: questions,
+          log: log
+        });
+      } catch (error) {
+        e = error;
+        return $q.reject(e);
+      }
     };
 
-    // Ugly as hell, but it will do for now. I'm too lazy to wrap it properly.
-    var originalLoadFromString = ret.loadFromString;
-    ret.loadFromString = function (input) {
-        try {
-            return originalLoadFromString(input);
-        } catch (e) {
-            return $q.reject(e);
-        }
-    };
+    return _Class;
 
-    return ret;
+  })());
 });

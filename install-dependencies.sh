d91be87345069b983ec78b5dc32594633b0b6bbc
Krzysztof Śmiałek <admin@avensome.net>
2025-02-21 19:35:11 +0000

    Add Github workflow for PRs

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,3 +6,5 @@ GULP_VERSION="$(jq -r .packages['""'].devDependencies.gulp package-lock.json)"
 BOWER_VERSION="$(jq -r .packages['""'].devDependencies.bower package-lock.json)"
 
 npm i -g "gulp@$GULP_VERSION" "bower@$BOWER_VERSION"
+gulp --version
+bower --version

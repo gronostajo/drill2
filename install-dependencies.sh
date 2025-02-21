ec9eb4557c75b586cfec4bee95be777da73086f7
Krzysztof Śmiałek <admin@avensome.net>
2025-02-21 19:17:31 +0000

    Add VS Code dev container

---
+++ install-dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -xe
+
+GULP_VERSION="$(jq -r .packages['""'].devDependencies.gulp package-lock.json)"
+BOWER_VERSION="$(jq -r .packages['""'].devDependencies.bower package-lock.json)"
+
+npm i -g "gulp@$GULP_VERSION" "bower@$BOWER_VERSION"

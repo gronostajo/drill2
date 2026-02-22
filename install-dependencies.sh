e7ec7c7e2216ca960e548c60944c27e43843ba36
Krzysztof Śmiałek <admin@avensome.net>
2026-02-22 23:31:09 +0000

    Fix devcontainer

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,8 +2,8 @@
 
 set -xe
 
-GULP_VERSION="$(jq -r .packages['""'].devDependencies.gulp package-lock.json)"
-BOWER_VERSION="$(jq -r .packages['""'].devDependencies.bower package-lock.json)"
+GULP_VERSION="$(jq -r .dependencies.gulp package-lock.json)"
+BOWER_VERSION="$(jq -r .dependencies.bower package-lock.json)"
 
 npm i -g "gulp@$GULP_VERSION" "bower@$BOWER_VERSION"
 gulp --version
